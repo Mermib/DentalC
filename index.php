fe45825627989b0c1e0c53ddd719155198745aeb
Mercy <46165433+Mermib@users.noreply.github.com>
2021-07-31 13:24:47 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dental Clinic | Hi - tech Dental Solutions</title>
+    <link rel="stylesheet" href="fonts/stylesheet.css" />
+    <link rel="stylesheet" href="css/styles.css" />
+    <link rel="stylesheet" media="(min-width:600px)" href="css/desktop.css" />
+</head>
+<body>
+    <header>
+        <div class="container">            
+            <div class="sides tollfree"> 
+                <span>Call us</span>
+                <span>Toll free</span>
+                <span>-1 (866) 953 0710</span>
+            </div>
+            <div class="logo-img">
+                <img src="img/logo.jpg" />
+            </div>
+            <div class="sides hours-service">
+                <span>Hours of service</span>
+                <span><strong>Monday to Friday 9:00am - 7:00pm</span>
+                <span>Saturday: 9:00am - 1:30pm</strong></span>
+            </div>
+        </div>
+    </header>
+    <main class="">
+        <section class="titles">
+            <h1>Best All On 4 Dental Implants In Mexico</h1>
+            <p>Experience, expertise and innovation (more than 10,000 treatments).</p>
+        </section>
+
+        <section class="best-deal-container">
+            <div class="best-deal"></div>
+            <div class="info-here"> 
+                <span>Request more information for FREE</span>
+                <button id="myBtn" class="openModal">Contact Us!</button>
+            </div>
+        </section>
+
+        <section class="services-plus">
+
+        </section>
+
+        <section class="best-deal-container">
+            <div class="best-deal"></div>
+            <div class="info-here"> 
+                <span>Request more information for FREE</span>
+                <button id="myBtn"  class="openModal">Contact Us!</button>
+            </div>
+        </section>
+
+        <section class="services">
+            <div class="service"></div>
+            <div class="service"></div>
+        </section>
+
+        <section class="reviews">
+            <div class="review"></div>
+            <div class="review"></div>
+            <div class="review"></div>
+        </section>
+
+        <section class="cta">
+
+        </section>
+
+        <section class="faqs">
+
+        </section>
+    </main>
+    <footer>
+
+    </footer>
+
+
+    <!-- The Modal -->
+    <div id="contactModal" class="modal">
+
+    <!-- Modal content -->
+    <div class="modal-content">
+        <span class="close">&times;</span>
+        <h2>We want to see you smile!</h2>
+        <p>Leave your contact and we'll be in touch as soon as possible to personally clear all your doubts concerns!</p>
+
+        <form>
+            <fieldset>
+                <label for="email">E-mail:</label>
+                <input type="email" id="email" name="email">
+            </fieldset>
+            <fieldset>
+                <label for="phone">Telephone</label>
+                <input type="text" id="phone" name="phone">
+            </fieldset>
+            <p>Select which one do you preffer for us to contact you:</p>
+            <div>
+                <fieldset class="radio-options ">               
+                    <input type="radio" id="contactemail" name="contactemail" value="contactemail">
+                    <label for="contactemail">E-mail</label>
+                </fieldset>
+                <fieldset class="radio-options ">               
+                    <input type="radio" id="telephone" name="telephone" value="telephone">
+                    <label for="telephone">Telephone</label>
+                </fieldset>
+            </div>
+            <input type="submit" value="Send">
+        </form>       
+    </div>
+
+    <script type="text/javascript" src="js/modal.js" async></script>
+
+</div>
+</body>
+</html>
