d6ffb94fd384812ffd89296e138f37073c4b774d
Mercy <46165433+Mermib@users.noreply.github.com>
2021-07-31 17:55:08 -0500

    avance dos

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
                 <span>-1 (866) 953 0710</span>
             </div>
             <div class="logo-img">
-                <img src="img/logo.jpg" />
+                <img src="img/Logo_dental_Clinic.png" alt="" width="100%" />
             </div>
             <div class="sides hours-service">
                 <span>Hours of service</span>
@@ -41,8 +41,25 @@
             </div>
         </section>
 
-        <section class="services-plus">
-
+        <section class="services-plus container">
+            <h2>What make us different?</h2>
+            <div class="plus">
+                <div class="">
+                    <span><object type="image/svg+xml" data="img/shield.svg" class="shield">  </object></span>
+                    <h3>Safety</h3>
+                    <p>Our professional team it's one of the most recognized.</p>
+                </div>
+                <div class="">
+                    <span><object type="image/svg+xml" data="img/tech.svg" class="shield">  </object></span>
+                    <h3>Technology</h3>
+                    <p>We only use the best equipment that will bring the best results.</p>
+                </div>
+                <div class="">
+                    <span><object type="image/svg+xml" data="img/diamond.svg" class="shield">  </object></span>
+                    <h3>Service</h3>
+                    <p>We kind all the customers with kind and greatfulness.</p>
+                </div>
+            </div>
         </section>
 
         <section class="best-deal-container">
@@ -53,27 +70,105 @@
             </div>
         </section>
 
-        <section class="services">
-            <div class="service"></div>
-            <div class="service"></div>
+
+        <h2 class="textcenter">We have the best services</h2>
+        <section class="services">            
+            <div class="services-container container">
+                <div class="service">
+                    <img src="img/Image_3.jpg" alt=""  width="100%"  />
+                    <h4>The ultimate dental implant treatment</h4>
+                    <p>Dr. Morales the top dental implant specialist in Cancun, mini and full dental implants expert, 
+                        and the best implant dentists in Mexico offer the newest and most advanced dental implants; 
+                        full mouth restoration options including mini dental implants and the latest ALL ON 4 and ALL ON 6
+                         dental implant procedures, used to restore the entire upper or lower arch of teeth in one complete bridge.</p>
+                </div>
+                <div class="service">
+                    <img src="img/Image_4.jpg" alt=""  width="100%"  />
+                    <h4>Advanced technology and first level facilities</h4>
+                    <p>We have an in-house Digital Lab and the best team of internationally trained dental specialists in the Caribbean, 
+                        committed to taking care of your smile and making you shine.</p>                 
+                </div>
+            </div>
         </section>
 
-        <section class="reviews">
-            <div class="review"></div>
-            <div class="review"></div>
-            <div class="review"></div>
+        <section class="reviews container">
+            <h2 class="textcenter">What people have to say about us.</div>
+            <div class="review">
+                <div><img src="img/Avatar_1.jpg" alt=""></div>
+                <div>
+                    <p><strong>Jane Cooper.</strong> Ranchview, California</p>
+                    <p>"My neighbor Georgie has one of these. She works as a busboy and she says it loos brown. It only works when I'm Norway"</p>
+                </div>
+            </div>
+            <div class="review floatright">
+                <div><img src="img/Avatar_2.jpg" alt=""></div>
+                <div>
+                    <p><strong>Jenny Wilson.</strong> New York, New York</p>
+                    <p>"My neighbor Georgie has one of these. She works as a busboy and she says it loos brown. It only works when I'm Norway"</p>
+                </div>
+            </div>
+            <div class="review clear">
+                <div><img src="img/Avatar_3.jpg" alt=""></div>
+                <div>
+                    <p><strong>Guy Hawkings.</strong> Austin, Texas</p>
+                    <p>"My neighbor Georgie has one of these. She works as a busboy and she says it loos brown. It only works when I'm Norway"</p>
+                </div>
+            </div>
         </section>
 
-        <section class="cta">
-
+        <section class="cta container">
+                <div>
+                    <h2>We want to see you smile!</h2>
+                    <p>Feel free to give us a call and we will answer all your questions with no charge or compromise.</p>
+                    <button class="openModal">I want more detals</button>
+                </div>
+                <div class="imgcta">
+                    <img src="img/Image_5.jpg" alt="" width="100%" />
+                </div>
         </section>
 
         <section class="faqs">
-
+            <div class="container">
+                <h2>Frecuently Asked Questions</h2>
+                <div class="faqs-container">
+                    <div class="question">
+                        <a href="#">What are All-on-4 dental implants?</a>
+                        <div>All-on-Four treatment involves essentially the replacement of all missing teeth in one set of teeth by using only 4
+                            (sometimes 6 or 8) implants.</div>
+                    </div>   
+                    <div class="question">
+                        <a href="#">Who is candidate for All-on-4 dental implants?</a>
+                        <div>Almost anyone who is missing multiple teeth, or all teeth are candidates for All-on-4 dental implants.
+                        What are the advantages of All-on-4 procedure?</div>
+                    </div>  
+                    <div class="question">
+                        <a href="#">You can have a total dental restoration with only 4 implants, so the procedure is not as invasive as others.</a>
+                        <div>A.- Often, bone grafting is not necessary.</div>
+                    </div>  
+                    <div class="question">
+                        <a href="#">How much do All-on 4 dental implants cost?</a>
+                        <div>The approximate cost per arch is about 12,000 usd in the USA. However, you can go to highly qualified clinics in Cancun Mexico, 
+                            where the same technology is used, same quality of materials but the cost is considerably lower, almost 40%.</div>
+                    </div>  
+                    <div class="question">
+                        <a href="#">Do All-on 4 dental implants last?</a>
+                        <div>The approximate cost per arch is about 12,000 usd in the USA. However, you can go to highly qualified clinics in Cancun Mexico,
+                        where the same technology is used, same quality of materials but the cost is considerably lower, almost 40%.</div>
+                    </div>  
+                    <div class="question">
+                        <a href="#">Are All-on 4 dental implants removable?</a>
+                        <div>No.</div>
+                    </div>         
+                </div>
+            </div>
         </section>
     </main>
     <footer>
-
+        <div class="logo-footer">
+            <img src="img/Logo_dental_Clinic.png" alt="" width="100%"/>
+        </div>
+        <a href="https://www.dentalcliniccancun.com">www.dentalcliniccancun.com</a>
+        <a href="mailto:consultas@dentalcliniccancun.com">consultas@dentalcliniccancun.com</a>
     </footer>
 
 
